55c98d4746b5d0178416b25ba1552a8bdaa84055
范晟祐 <chengyu@fanchengyousmbp.lan>
2022-06-07 02:38:52 -0500

    Remove redefined sphere

--- src/main.cc
+++ src/main.cc
@@ -50,7 +50,6 @@ hittable_list random_scene() {
                     auto center2 = center + vec3(0, random_double(0,.5), 0);
                     world.add(make_shared<moving_sphere>(
                         center, center2, 0.0, 1.0, 0.2, sphere_material));
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5, 1);
